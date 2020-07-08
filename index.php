2aef172eaf03bbc6e330e75c07c7a386256adf3d
MuhammadNurHidayat07092000 <yyt08536@gmail.com>
2020-07-08 19:50:53 +0800

    menambahkan paragraf

--- index.php
+++ index.php
@@ -7,5 +7,6 @@
 </head>
 <body>
     <h1>Hallo Word!</h1>
+    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iure delectus numquam illo neque quia libero maxime sit officiis optio expedita aut, asperiores omnis dolore. Saepe iste delectus eos ipsa officiis.</p>
 </body>
 </html>
